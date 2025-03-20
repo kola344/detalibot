934d94293d48f465496117442d5befeea180118a
kolaparsin <koliaparshin@yandex.ru>
2025-03-20 15:03:23 +0300

    init commit

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 bot_token = '7523528768:AAHJQ7uDKPBN3TqVI9VXb3y2uFHDf880ywA'
-db_url = 'postgresql://localhost:5432/detalibot'
+db_url = 'postgresql://admin@localhost:5432/detalibot'
 price = 1990
 webhook_url = 'https://remontkvartirms.ru'
 
