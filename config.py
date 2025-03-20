c561dcb78aa6b7e7a471087f1229277f4a74a3bb
kolaparsin <koliaparshin@yandex.ru>
2025-03-20 10:26:47 +0300

    init commit

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 bot_token = '7523528768:AAHJQ7uDKPBN3TqVI9VXb3y2uFHDf880ywA'
 db_url = 'postgresql://localhost:5432/detalibot'
 price = 1990
-webhook_url = ''
+webhook_url = 'http://109.172.7.171'
 
 adm_ids = [1659397548]
