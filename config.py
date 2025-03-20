bacb3d4d7982ed174a8a2cf2eff65d37321d7d31
kolaparsin <koliaparshin@yandex.ru>
2025-03-20 15:36:34 +0300

    init commit

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 bot_token = '7523528768:AAHJQ7uDKPBN3TqVI9VXb3y2uFHDf880ywA'
 db_url = 'postgresql://admin@localhost:5432/detalibot'
 price = 1990
-webhook_url = 'https://remontkvartirms.ru'
+webhook_url = 'https://detalibot.kafevirazh.ru'
 
 adm_ids = [1659397548, 5898749605, 1024257520]
